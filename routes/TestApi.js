5b5ea4940e9db962e37211d8bbc6d5fdaa8a4ca2
Zain-mir24 <zainnaeemk10@gmail.com>
2021-07-27 09:48:01 -0700

    insetion into database using react, Still in progress

--- routes/TestApi.js
+++ routes/TestApi.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require("mysql");
+var connection = mysql.createConnection({
+  //properties
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'globalreach'
 
-/* GET Api testing. */
-router.get('/', function(req, res, next) {
-  res.send('Testing the Api');
+})
+connection.connect(function(error){
+  if(!error){
+      console.log('Error')
+  }
+  else{
+      console.log('Connected')
+  }
+})
+/* GET home page. */
+router.post('/add', function(req, res, next) {
+  res.send('this is the mainpage');
+  const fname=req.body.Firstname
+  const lname=req.body.Lastname
+  const email=req.body.Email
+  const password=req.body.Password
+  console.log(req.body)
+  connection.query("INSERT INTO `signup`(`Firstname`, `Lastname`, `Email`, `Password`) VALUES (?,?,?,?)",[fname,lname,email,password],function(error,rows,field){
+    if(!!error){
+        console.log('Error in  the query')
+    }
+    else{
+     console.log('successful query')
+    }
+ })
 });
 
+
+  
 module.exports = router;
