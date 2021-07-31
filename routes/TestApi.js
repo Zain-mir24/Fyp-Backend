d760a8e516040d0d3880025133fe16195f4572c0
Zain-mir24 <zainnaeemk10@gmail.com>
2021-07-31 11:25:00 -0700

    search done

--- routes/TestApi.js
+++ routes/TestApi.js
@@ -6,43 +6,56 @@ let query = require("../libs/sql");
 router.post("/add", async (req, res, next) => {
   try {
     //getting data from input fields
-  const Firstname = req.body.Firstname;
-  const Lastname = req.body.Lastname;
+    const Firstname = req.body.Firstname;
+    const Lastname = req.body.Lastname;
+    const Email = req.body.Email;
+    const Password = req.body.Password;
+
+    //query for selection email
+
+    //if not then enter the data into the database
+
+    const ifEmail = await query(
+      `SELECT Email FROM signup WHERE Email = "${Email}"`
+    );
+    if (ifEmail.length > 0) {
+      console.log("ifEMail", ifEmail);
+      return res.status(409).json(ifEmail);
+    }
+
+    const addEmail = await query(
+      "INSERT INTO signup (Firstname, Lastname, Email, Password) VALUES (?,?,?,?)",
+      [Firstname, Lastname, Email, Password]
+    );
+
+    if (!addEmail) {
+      return res.status(400).json({ message: "Not saved" });
+    }
+
+    return res.status(200).json(addEmail);
+  } catch (err) {
+    console.log("err", err);
+    next(err);
+  }
+});
+router.post("/search", async (req, res, next) => {
   const Email = req.body.Email;
-  const Password = req.body.Password;
+  let Firstname, Lastname, Password;
 
-  //query for selection email
-
-  //if not then enter the data into the database
-
-  const ifEmail = await query(`SELECT Email FROM signup WHERE Email = "${Email}"`);
+  const ifEmail = await query(
+    `SELECT Firstname,Lastname,Email,Password FROM signup WHERE Email = "${Email}"`
+  );
   if (ifEmail.length > 0) {
     console.log("ifEMail", ifEmail);
-    return res.status(409).json(ifEmail);
-  } 
+    Firstname = ifEmail[0].Firstname;
+    Lastname = ifEmail[0].Lastname;
+    Password = ifEmail[0].Password;
+    console.log(Firstname);
+    console.log(Lastname);
+    console.log(Password);
 
-  const addEmail = await query(
-    "INSERT INTO signup (Firstname, Lastname, Email, Password) VALUES (?,?,?,?)",
-    [Firstname, Lastname, Email, Password]
-  )
-
-  if(!addEmail) {
-    return res.status(400).json({message: "Not saved"});
+    return res.status(409).json(Firstname);
   }
-
-  return res.status(200).json(addEmail);
-
-
-
-  } catch(err) {
-    console.log("err", err);
-    next(err)
-  }
-  
-});
-router.post("/search", (req, res, next) => {
-  res.send({ type: "post" });
-  
 });
 
 module.exports = router;
