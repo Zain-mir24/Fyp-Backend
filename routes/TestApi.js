9a033254c6314e927f8e1212dbf5e2c72c797a8a
Zain-mir24 <zainnaeemk10@gmail.com>
2021-07-31 03:38:42 -0700

    both insertion and selection works

--- routes/TestApi.js
+++ routes/TestApi.js
@@ -11,31 +11,38 @@ router.post("/add", async function (req, res, next) {
   const Password = req.body.Password;
 
   //query for selection email
-  // query(
-  //   'SELECT Email FROM signup WHERE Email ="' + Email + '"', //checks if email already exist
-  //   function (err, result) {
-      // if (result) {
-        //if it does return that email already exist
-      //   res.send("Email exists already enter a new email");
-      //   console.log("already exists");
-      // } else {
-        //if not then enter the data into the database
-        query(
-          "INSERT INTO signup (Firstname, Lastname, Email, Password) VALUES (?,?,?,?)",
-          [Firstname, Lastname, Email, Password]
-        )
-          .then((val) => {
-            console.log("val", val);
-            return res.status(200).json({ body: val });
-          })
-          .catch((err) => {
-            console.log("err", err);
-            return res.status(400).json({ body: err });
-          });
-    //   }
-    // }
-//   );
+
+  //if not then enter the data into the database
+  
+    query('SELECT COUNT(*) AS cnt FROM signup WHERE Email ="' + Email + '"',function(err,data){
+      if(err){
+        console.log(err);
+    }   
+    else{
+        if(data[0].cnt > 0){  
+              res.send("Already exist")
+        }else{
+          query(
+            "INSERT INTO signup (Firstname, Lastname, Email, Password) VALUES (?,?,?,?)",
+            [Firstname, Lastname, Email, Password]
+          )
+            .then((val) => {
+              console.log("val", val);
+              return res.status(200).json({ body: val });
+            })
+            .catch((err) => {
+              console.log("err", err);
+              return res.status(400).json({ body: err });
+            });
+
+ 
+  }
+}
+
+  
+  
 });
+})
 router.post("/search", function (req, res, err, result) {
   res.send({ type: "post" });
   // const Email = req.body.Email;
