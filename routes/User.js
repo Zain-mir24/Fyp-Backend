1d30b8eb08206fd57a060a2b2037b69c793e2277
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-03 10:20:10 -0700

    user route and admin routes separated

--- routes/User.js
+++ routes/User.js
@@ -1,12 +1,12 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 let express = require("express");
 let router = express.Router();
 let query = require("../libs/sql");
 
-const  auth= require("../middleware/auth") 
-const User=require("../models/users")
-const mongoose =require("../db/mongoose")
+const auth = require("../middleware/auth");
+const User = require("../models/users");
+const mongoose = require("../db/mongoose");
 /* POST Signup */
 
 router.post("/add", async (req, res, next) => {
@@ -14,43 +14,34 @@ router.post("/add", async (req, res, next) => {
 
   try {
     await user.save();
-    
+
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
-    console.log('errrorr',e)
+    console.log("errrorr", e);
     res.status(400).send(e);
   }
-  
 });
-//this route is for the admin. To search and get the data of the User
-router.post("/search",auth, async (req, res, next) => {
-  const Email = req.body.Email;
-  let Id,Firstname, Lastname, Password;
-  const ifEmail = await query(
-    `SELECT id,firstname,lastname,email,password FROM signup WHERE email = "${Email}"`
-  );
-  if (ifEmail.length > 0) {
-    console.log("ifEmail", ifEmail);
-    Id =ifEmail[0].id
-    Firstname = ifEmail[0].Firstname;
-    Lastname = ifEmail[0].Lastname;
-    Password = ifEmail[0].Password;
-    const User = {
-      Firstname,
-      Lastname,
-      Email,
-      Password,
-    };
-    return res.status(409).json(User);
+
+//user login route
+// Post route
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = user.generateAuthToken();
+    await user.save();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(409).send(e);
   }
 });
-router.post("/login",async(req,res)=>{
-  try{
-
-  }catch(e){
-
-  }
-})
-
+//logout route for the user
+//post route for the user
+router.post("/logout", async (req, res) => {
+  try {
+  } catch (e) {}
+});
 module.exports = router;
