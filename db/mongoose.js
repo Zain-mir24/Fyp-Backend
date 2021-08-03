995cd155a4e51414856ea80acb0d7371e13f0e15
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-08-03 16:33:57 +0500

    testcommit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://admin:globalreach123@cluster0.kdgc0.mongodb.net/Test', {
+mongoose.connect(
+  "mongodb+srv://admin:globalreach123@cluster0.kdgc0.mongodb.net/Test",
+  {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true 
-})
+    useUnifiedTopology: true,
+  }
+);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://admin:globalreach123@cluster0.kdgc0.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('this is the mainpage');
+router.get("/", function (req, res, next) {
+  res.send("this is the mainpage");
+  const Cat = mongoose.model("Cat", { name: String });
+
+  const kitty = new Cat({ name: "Zildjian" });
+  kitty.save().then(() => console.log("meow"));
 });
 
-
-  
 module.exports = router;
