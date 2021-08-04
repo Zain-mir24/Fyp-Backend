c6deed6ceb22f45a15daaf24d29b384f828f4be6
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-04 10:54:27 -0700

    3 routes of admin created

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,12 @@ dotenv.config();
 let express = require("express");
 let router = express.Router();
 let query = require("../libs/sql");
-
 const  auth= require("../middleware/auth") 
 const User=require("../models/users")
 const mongoose =require("../db/mongoose")
 //Admin routes
+
+//viewing all the users
 router.get("/user", async (req, res, next) => {
  try{
    const user =await User.find({})
@@ -16,5 +17,27 @@ router.get("/user", async (req, res, next) => {
  res.status(501).send()
  }
 });
-  
+
+//updating the user in the database
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+//deleting user from the database
+
 module.exports = router;
