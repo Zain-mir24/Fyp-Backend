9b36f36a32b6a3a77ea454d114d52381a72a4c91
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-11 01:26:20 -0700

    work on admin routes

--- models/users.js
+++ models/users.js
@@ -68,7 +68,7 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, process.env.ACCESS_TOKEN_SECRET)
-
+       console.log(token)
     user.tokens = user.tokens.concat({ token })
     await user.save()
     
