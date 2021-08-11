5f63ec8f4ec2094ef697988ed822c0ea058ec534
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-11 02:25:06 -0700

    comments

--- routes/admin.js
+++ routes/admin.js
@@ -12,42 +12,43 @@ const mongoose = require("../db/mongoose");
 //updating the user in the database
 router.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
-  console.log(req.body )
+  console.log(req.body);
   console.log(updates);
-  const allowedUpdates = ['name', 'email', 'password'];
-  const isValidOperation = updates.every((update) =>{
-    return allowedUpdates.includes(update)
-  }   
-  );
+  const allowedUpdates = ["name", "email", "password"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
   if (!isValidOperation) {
     return res.status(400).send({ error: "Invalid updates!" });
   }
   try {
-    const user =await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
-    if(!user){
-      return res.status(404).send()
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
     }
-  
-   
+
     res.send(user);
   } catch (e) {
-    console.log("error",e);
+    console.log("error", e);
     res.status(400).send(e);
   }
 });
 //deleting user from the database
-router.delete('/users/:id', async (req, res) => {
+router.delete("/users/:id", async (req, res) => {
   try {
-      const user = await User.findOneAndDelete({ _id: req.params.id})
+    const user = await User.findOneAndDelete({ _id: req.params.id }); //checking if id is in the database
 
-      if (!user) {
-          res.status(404).send()
-      }
+    if (!user) {
+      res.status(404).send();
+    }
 
-      res.send(user)
+    res.send(user);
   } catch (e) {
-      res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
 module.exports = router;
