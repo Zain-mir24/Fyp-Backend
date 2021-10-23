a27f77588f0f45f7c82c05b9e637c2c33208b025
Zain-mir24 <zainnaeemk10@gmail.com>
2021-10-23 10:40:52 -0700

    working backend and front end

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://Zain:globalreach123@cluster0.myxr4.mongodb.net/Globalreach?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true 
-})
+}).then(()=>{
+    console.log(`successfully connected`);
+    }).catch((e)=>{
+    console.log(`not connected`,e);
+    })
 module.exports=mongoose
