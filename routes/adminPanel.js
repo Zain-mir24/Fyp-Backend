f4db03a8413e38837f4b69a2174b5a4006a57cad
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-12-14 02:33:50 +0500

    test

--- routes/adminPanel.js
+++ routes/adminPanel.js
@@ -16,34 +16,34 @@ const jwt = require("jsonwebtoken");
 const mailSender = require("../email/account");
 
 // Verifying Email for  Latest News
-router.post("/saveEmail", async(req, res) => {
-  console.log(req.body,"frontend")
-  const email = new Email(req.body)
-  const email1=await Email.findOne({ Email: req.body.Email })
- try{ 
-  if(!email1){
-    const link= await email.verifyToken();
-    console.log(link)
-    const mailOptions = {
-      from: '"Our Code World " <zainzz123@outlook.com>',
-      to: email,
-      subject: "Verify your email",
-      text: `Here is your verification link ${link}`,
-    };
-    await mailSender.transporter.sendMail(mailOptions);
-    res.status(201).send("Mailsent");
-  }}
-  catch(e){
-    console.log(e)
-    res.status(500).send(e)
+router.post("/saveEmail", async (req, res) => {
+  console.log(req.body, "frontend");
+  const email = new Email(req.body);
+  const email1 = await Email.findOne({ Email: req.body.Email });
+  try {
+    if (!email1) {
+      const link = await email.verifyToken();
+      console.log(link);
+      const mailOptions = {
+        from: '"Our Code World " <zainzz123@outlook.com>',
+        to: email,
+        subject: "Verify your email",
+        text: `Here is your verification link ${link}`,
+      };
+      await mailSender.transporter.sendMail(mailOptions);
+      res.status(201).send("Mailsent");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
   }
 });
 // saving verified email
-router.post("/saveEmail/:id/:token",async(req,res)=>{
-  const {token}=req.params;
-  console.log(req.body)
-  const email= new Email(req.body);
-  console.log(email.Email)
+router.post("/saveEmail/:id/:token", async (req, res) => {
+  const { token } = req.params;
+  console.log(req.body);
+  const email = new Email(req.body);
+  console.log(email.Email);
   const secret = process.env.ACCESS_TOKEN_SECRET + email.Email;
   try {
     const payload = await jwt.verify(token, secret);
@@ -53,12 +53,12 @@ router.post("/saveEmail/:id/:token",async(req,res)=>{
       return res.render("doesnt work");
     }
     await email.save();
-    res.status(201).send({email});
+    res.status(201).send({ email });
     console.log("User saved");
   } catch (e) {
     console.log(e, "error while saving");
   }
-})
+});
 router.get("/displayEmail", async (req, res) => {
   try {
     const email = await Email.find();
@@ -69,16 +69,23 @@ router.get("/displayEmail", async (req, res) => {
 });
 
 router.post("/sendAllEmail", async (req, res) => {
-  var exp = await Email.find();
-  exp.map((item) => {
-    const mailOptions = {
-      from: req.body.from,
-      to: item.Email,
-      subject: req.body.subject,
-      text: req.body.message,
-    };
-    mailSender.transporter.sendMail(mailOptions);
-  });
+  try {
+    var exp = await Email.find();
+    exp.map((item) => {
+      console.log(item.Email);
+    });
+    exp.map(async (item) => {
+      const mailOptions = {
+        from: req.body.from,
+        to: item.Email,
+        subject: req.body.subject,
+        text: req.body.message,
+      };
+      await mailSender.transporter.sendMail(mailOptions);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   res.status(200).send("OK");
 });
