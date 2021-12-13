60b3850b7979df6751569e2b4a800d539119c87c
Zain-mir24 <zainnaeemk10@gmail.com>
2021-12-13 21:53:06 +0500

    email verification

--- routes/adminPanel.js
+++ routes/adminPanel.js
@@ -11,21 +11,54 @@ const mongoose = require("../db/mongoose");
 const News = require("../models/LatestNewsDB");
 const upload = require("../middleware/img");
 const fs = require("fs");
-const { response } = require("express");
+const jwt = require("jsonwebtoken");
+
 const mailSender = require("../email/account");
 
-router.post("/saveEmail", (req, res) => {
-  Email.findOne({ Email: req.body.Email }).then((result) => {
-    if (result == null) {
-      Email.create(req.body).then((response) => {
-        res.status(200).send(response);
-      });
-    } else {
-      res.status(300).send("Email Exist");
-    }
-  });
+// Verifying Email for  Latest News
+router.post("/saveEmail", async(req, res) => {
+  console.log(req.body,"frontend")
+  const email = new Email(req.body)
+  const email1=await Email.findOne({ Email: req.body.Email })
+ try{ 
+  if(!email1){
+    const link= await email.verifyToken();
+    console.log(link)
+    const mailOptions = {
+      from: '"Our Code World " <zainzz123@outlook.com>',
+      to: email,
+      subject: "Verify your email",
+      text: `Here is your verification link ${link}`,
+    };
+    await mailSender.transporter.sendMail(mailOptions);
+    res.status(201).send("Mailsent");
+  }}
+  catch(e){
+    console.log(e)
+    res.status(500).send(e)
+  }
 });
-
+// saving verified email
+router.post("/saveEmail/:id/:token",async(req,res)=>{
+  const {token}=req.params;
+  console.log(req.body)
+  const email= new Email(req.body);
+  console.log(email.Email)
+  const secret = process.env.ACCESS_TOKEN_SECRET + email.Email;
+  try {
+    const payload = await jwt.verify(token, secret);
+    console.log("token verified");
+    if (!payload) {
+      console.log("user not saved because token was not verified");
+      return res.render("doesnt work");
+    }
+    await email.save();
+    res.status(201).send({email});
+    console.log("User saved");
+  } catch (e) {
+    console.log(e, "error while saving");
+  }
+})
 router.get("/displayEmail", async (req, res) => {
   try {
     const email = await Email.find();
