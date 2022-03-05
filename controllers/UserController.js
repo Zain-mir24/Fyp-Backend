5a6de6c3e8460206f16878c7755f6e0b0b70727f
Zain-mir24 <zainnaeemk10@gmail.com>
2022-03-05 17:31:09 +0500

    daata being sent

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,6 +17,7 @@ const viewChildren = async (req, res, next) => {
   }
 }
 const monthlyAppeal = async (req, res, next) => {
+
   var obj = {
     bid: req.body.bid,
     phoneNumber: req.body.phoneNumber,
@@ -27,30 +28,26 @@ const monthlyAppeal = async (req, res, next) => {
     Totalincome: req.body.Totalincome,
     Totalexpenses: req.body.Totalexpenses,
     NativeTown: req.body.NativeTown,
-
     accomodationself: req.body.self,
     accomodationdonated: req.body.donated,
     accomodationrental: req.body.rental,
     accomodationrent: req.body.rent,
+    widowfamdetail: JSON.parse(req.body.widowfamdetail).map((i) => ({
 
-    widowfamdetail: [
-      {
-        name: req.body.widowfamdetail[0].name,
-        age: req.body.widowfamdetail[0].age,
-        relation: req.body.widowfamdetail[0].relation,
-        activities: req.body.widowfamdetail[0].activities,
-        income: req.body.widowfamdetail[0].income,
-      },
-    ],
-    widowsibilings: [
-      {
-        name: req.body.widowsibilings[0].name,
-        age: req.body.widowsibilings[0].age,
-        relation: req.body.widowsibilings[0].relation,
-        activities: req.body.widowsibilings[0].activities,
-        income: req.body.widowsibilings[0].income,
-      },
-    ],
+      name: i.name,
+      age: i.age,
+      relation: i.relation,
+      activities: i.activities,
+      income: i.income
+    })),
+    widowsibilings: JSON.parse(req.body.widowsibilings).map((i) => ({
+
+      name: i.name,
+      age: i.age,
+      relation: i.relation,
+      activities: i.activities,
+      income: i.income
+    })),
     medicineCost: req.body.medicineCost,
 
     bformname: req.body.bformname,
@@ -59,8 +56,6 @@ const monthlyAppeal = async (req, res, next) => {
     totalamountdonation: req.body.totalamountdonation,
   }
   try {
-    console.log(req.body.widowfamdetail[0].name, "name pakarlo")
-
     const newAppeal = await Monthly.create(obj)
     if (!newAppeal) {
       next("error while creating the appeal")
