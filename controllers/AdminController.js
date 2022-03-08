c053a1f5d49f128af915b889678fc8e3b8da6bcc
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2022-03-08 10:05:50 +0500

    H

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -3,10 +3,10 @@ const User = require("../models/users");
 const Donation = require("../models/Donation");
 const appeal = require("../models/appealedCampaign");
 const Admin = require("../models/Admin");
-const Monthly = require("../models/MonthlySupport")
-const Amount = require("../models/AmountDetail")
-const Housing = require("../models/HousingScheme")
-const Estimation = require("../models/Estimationperforma")
+const Monthly = require("../models/MonthlySupport");
+const Amount = require("../models/AmountDetail");
+const Housing = require("../models/HousingScheme");
+const Estimation = require("../models/Estimationperforma");
 // Admin signup routes and addition of admins
 const SuperAdmin = async (req, res, next) => {
   const { email } = req.body;
@@ -124,8 +124,8 @@ const changePassword = async (req, res, next) => {
 };
 // Adding Children data to database
 const addChild = async (req, res, next) => {
-  const myChild = new children(req.body)
-  console.log(myChild, "child's data coming from the frontend")
+  const myChild = new children(req.body);
+  console.log(myChild, "child's data coming from the frontend");
   try {
     await myChild.save();
     // const done = await children.create(req.body);
@@ -248,87 +248,100 @@ const deleteLoanApproved = async (req, res, next) => {
 // view monthly support of beneficiary
 const viewmonthlyAppeal = async (req, res, next) => {
   try {
-    const viewAppeal = await Monthly.find({}).populate("bid")
-    console.log(viewAppeal, "view")
-    res.status(200).send(viewAppeal)
+    const viewAppeal = await Monthly.find({}).populate("bid");
+    console.log(viewAppeal, "view");
+    res.status(200).send(viewAppeal);
+  } catch (e) {
+    res.status(500).send(e);
   }
-  catch (e) {
-    res.status(500).send(e)
-
-  }
-}
+};
 // Adding amount detail for  beneficiary
 const addamountDetail = async (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
   try {
-    const Amountdetail = await Amount.create(req.body)
-    res.status(200).send(Amountdetail)
+    const Amountdetail = await Amount.create(req.body);
+    res.status(200).send(Amountdetail);
+  } catch (e) {
+    res.status(500).send(e);
   }
-  catch (e) {
-    res.status(500).send(e)
-
-  }
-}
+};
 // viewing amount detail from each beneficiary
 const viewammountDetail = async (req, res, next) => {
   try {
-    const view = await Amount.find({}).populate("bid")
-    res.status(200).send(view)
+    const view = await Amount.find({}).populate("bid");
+    res.status(200).send(view);
   } catch (e) {
-    console.log(e)
-    res.status(500).send(e)
-
+    console.log(e);
+    res.status(500).send(e);
   }
-}
+};
 
 // Adding housing Scheme for beneficiary
 const addHousingScheme = async (req, res, next) => {
   try {
-    const add = await Housing.create(req.body)
+    const add = await Housing.create(req.body);
     if (!add) {
-      throw new Error("this format is wrong my man")
+      throw new Error("this format is wrong my man");
     }
-    res.status(200).send(add)
+    res.status(200).send(add);
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send(e);
   }
-}
+};
 // viewing housing Scheme for beneficiary
 const viewHousingScheme = async (req, res, next) => {
   try {
-    const view = await Housing.find({})
+    const view = await Housing.find({});
     if (!view) {
-      throw new Error("this format is wrong my man")
+      throw new Error("this format is wrong my man");
     }
-    res.status(200).send(view)
-
+    res.status(200).send(view);
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send(e);
   }
-}
+};
 const addEstimation = async (req, res, next) => {
   try {
-    const add = await Estimation.create(req.body)
-    if (!add) {
-      throw new Error("this format is wrong ")
-    }
-    res.status(200).send(add)
+    console.log(req.body);
+    const add = await Estimation.create(
+      {
+        Project: req.body.project,
+        Location: req.body.location,
+        Caretaker: req.body.caretaker,
+        Cellno: req.body.cellno,
+        Date: req.body.Date,
+        Material: req.body.Material,
+        Masoncharges: req.body.Masoncharges,
+        Labourcharges: req.body.Labourcharges,
+        Electriciancharges: req.body.Electriciancharges,
+        Plumbercharges: req.body.Plumbercharges,
+        Shutteringcharges: req.body.Shutteringcharges,
+        Paintercharges: req.body.Paintercharges,
+        Total: req.body.Total,
+      },
+      (res, err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(res);
+        }
+      }
+    );
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send(e);
   }
-}
+};
 const viewEstimation = async (req, res, next) => {
   try {
-    const view = await Estimation.find({})
+    const view = await Estimation.find({});
     if (!view) {
-      throw new Error("this format is wrong my man")
+      throw new Error("this format is wrong my man");
     }
-    res.status(200).send(view)
-
+    res.status(200).send(view);
   } catch (e) {
-    res.status(500).send(e)
+    console.log(e);
   }
-}
+};
 module.exports = {
   addHousingScheme,
   addEstimation,
--- models/Estimationperforma.js
+++ models/Estimationperforma.js
@@ -1,61 +1,56 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const EstimationSchema = new mongoose.Schema({
-    Project: {
-        type: String,
-        required: "true"
+  Project: {
+    type: String,
+    required: "true",
+  },
+  Location: {
+    type: String,
+    required: true,
+  },
+  Caretaker: {
+    type: String,
+    required: true,
+  },
+  Cellno: {
+    type: Number,
+    required: true,
+  },
+  Date: {
+    type: String,
+    required: true,
+  },
+  Material: [
+    {
+      name: { type: String, required: true },
+      AU: { type: String, required: true },
+      QTY: { type: Number, required: true },
+      Rate: { type: Number, require: true },
+      Cost: { type: Number, reqired: true },
     },
-    Location: {
-        type: String,
-        required: true
-    },
-    Caretaker: {
-        type: String,
-        required: true
-    },
-    Cellno: {
-        type: String,
-        required: true
-    },
-    Date: {
-        type: String,
-        required: true
-    },
-    Material: [{
-        name: { type: String, required: true },
-        AU: { type: String, required: true },
-        QTY: { type: Number, required: true },
-        Rate: { type: Number, require: true, },
-        Cost: { type: Number, reqired: true }
-    }],
-    Masoncharges: {
-
-        type: Number
-
-    },
-    Labourcharges: {
-
-        type: Number
-
-    },
-    Electriciancharges: {
-        type: Number
-
-    },
-    Plumbercharges: {
-        type: Number
-    },
-    Shutteringcharges: {
-
-        type: Number
-
-    },
-    Paintercharges: {
-        type: Number
-
-    },
-    Total: {
-        type: Number,
-        required: true
-    }
-})
-const Estimation = mongoose.model("EstimationSchema", EstimationSchema)
+  ],
+  Masoncharges: {
+    type: Number,
+  },
+  Labourcharges: {
+    type: Number,
+  },
+  Electriciancharges: {
+    type: Number,
+  },
+  Plumbercharges: {
+    type: Number,
+  },
+  Shutteringcharges: {
+    type: Number,
+  },
+  Paintercharges: {
+    type: Number,
+  },
+  Total: {
+    type: Number,
+    required: true,
+  },
+});
+const Estimation = mongoose.model("EstimationSchema", EstimationSchema);
+module.exports = Estimation;
