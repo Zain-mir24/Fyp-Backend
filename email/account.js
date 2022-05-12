182fa96cfe3781930cc3415bed9f226ec7a0abef
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2022-05-12 23:34:39 +0500

    Fixed Email Issue

--- email/account.js
+++ email/account.js
@@ -1,15 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com",
-  port: 587,
-  secureConnection: false,
-  tls: {
-    ciphers: "SSLv3",
-  },
+  service: "Gmail",
+  secure: true,
   auth: {
-    user: "zainzz123@outlook.com",
-    pass: "test12345678",
+    user: "globalreach.donate@gmail.com",
+    pass: "globalreach123",
   },
 });
 
